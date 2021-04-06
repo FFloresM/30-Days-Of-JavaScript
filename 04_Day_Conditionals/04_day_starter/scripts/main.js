f545f2ce4bbffe5f80b20c711cd4e898b6aab0e2
Francisco <victoriafrancis2009@gmail.com>
2021-04-05 22:55:47 -0400

    ejercicios dia 4

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,25 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+let edad = prompt("ingrese su edad:")
+if (edad>=18) {
+    alert("Tienes la edad suficiente para manejar")
+} else {
+    alert(`a ud le faltan ${18-edad} años para conducir`)
+}
+
+let myAge = 27
+if (myAge > edad) {
+    alert(`Yo soy ${myAge - edad} mayor que tú`)
+} else {
+    alert(`Tú eres ${edad - myAge} mayor que yo`)
+}
+
+let a = 4
+let b = 3
+if (a>b) {
+    alert(`${a} es mayor que ${b}`)
+} else {
+    alert(`${b} es mayor que ${a}`)
+}
+
+a>b?console.log(`${a} es mayor que ${b}`):console.log(`${b} es mayor que ${a}`)
