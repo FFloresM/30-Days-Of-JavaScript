fcec341c383810fd92d83fbfcf59f3e91946af72
Francisco <victoriafrancis2009@gmail.com>
2021-04-06 00:12:46 -0400

    ejercicios dia 5

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,10 @@
 console.log(countries)
+console.log(webTechs)
 alert('Open the browser console whenever you work on JavaScript')
 alert('Open the console and check if the countries has been loaded')
+
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(' ')
+console.log(words)
+console.log(words.length)
