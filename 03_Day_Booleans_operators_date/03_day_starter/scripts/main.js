99938f720cce90a246a00c824f52befc37e5a848
Francisco <victoriafrancis2009@gmail.com>
2021-04-05 20:25:44 -0400

    ejercicios dia 3

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,5 @@
 // this is your main.js script
+let base = prompt("ingrese base del triangulo")
+let altura = prompt("ingrese altura del triángulo")
+let area = 0.5 * base * altura
+console.log(area)
