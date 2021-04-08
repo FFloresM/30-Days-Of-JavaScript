bc59be7b4ae0339eb2a8881b39f58669ee016d87
Francisco <victoriafrancis2009@gmail.com>
2021-04-08 01:06:56 -0400

    día 8: objects:level 2

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,79 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+}
+console.log(users)
+let max_skills = 0
+max_skill_user = ''
+for(const user in users){
+    skills = users[user].skills.length
+    if (skills > max_skills) {
+        max_skills = skills
+        max_skill_user = user
+    }
+}
+console.log("usuario con mayor cantidad de skills:",max_skills, max_skill_user)
+//alert('Open the console and check if the countries has been loaded')
+let count = 0
+for(const user in users) {
+    loggedIn = users[user].isLoggedIn
+    if(loggedIn)
+        count++
+}
+console.log("usuarios conectados:", count)
+
+count = 0
+for(const user in users){
+    points = users[user].points
+    if (points>=50) {
+        count++
+    }
+}
+console.log(`usuarios con puntaje mayor o igual a 50: ${count}`)
